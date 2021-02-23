9ecc47063f4255e9b1c415745affa3d0344112b1
Daniel Brady <daniel.13rady@gmail.com>
2021-02-23 11:29:31 -0500

    Import 'Programmer Dvorak (MacOS) [v2]' from Oryx Configurator

---
+++ keyboards/moonlander/keymaps/dabrady/config.h
@@ -0,0 +1,10 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef TAPPING_TERM
+#define TAPPING_TERM 185
+
+#define FIRMWARE_VERSION u8"60lbl/34o0G"
+#define RGB_MATRIX_STARTUP_SPD 60
